72aa66bd8fa6793c8127a17e79eb0fc959ab1833
Manuel Artero Anguita <manutero.developer@gmail.com>
2025-09-11 17:42:55 +0200

    style: minor glow to the logo

--- src/equinox-logo.js
+++ src/equinox-logo.js
@@ -1,6 +1,6 @@
-import styles from "./logo.module.css";
-
 import svgContent from "./logo_equinox_2025.svg?raw";
+
+import styles from "./logo.module.css";
 
 export function EquinoxLogo() {
   const logo = document.createElement("div");
