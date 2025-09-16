aef7963c8ec74b72e808482c3d5014a8989af229
Ismael Taboada Rodero <ismaeljose.taboadarodero@telefonica.com>
2025-09-16 16:59:09 +0200

    chore: fix styles

--- src/agenda-highlight.js
+++ src/agenda-highlight.js
@@ -1,19 +1,19 @@
 const interval = setInterval(() => {
   const now = new Date();
   const currentTime = now.getHours() * 60 + now.getMinutes();
-  document.querySelectorAll(".agenda .highlight").forEach((el) => el.classList.remove("highlight"));
-  document.querySelectorAll(".agenda .agenda-column").forEach((column) => {
+  document.querySelectorAll('.agenda .highlight').forEach((el) => el.classList.remove('highlight'));
+  document.querySelectorAll('.agenda .agenda-column').forEach((column) => {
     const day = column.dataset.day;
     if (!day) return;
-    const today = now.toISOString().split("T")[0];
+    const today = now.toISOString().split('T')[0];
     if (day !== today) return;
-    column.querySelectorAll(".time").forEach((timeElement) => {
-      const startTimeParts = timeElement.dataset.startTime.split(":");
+    column.querySelectorAll('.time').forEach((timeElement) => {
+      const startTimeParts = timeElement.dataset.startTime.split(':');
       const timeInMinutes = parseInt(startTimeParts[0], 10) * 60 + parseInt(startTimeParts[1], 10);
       const endTimeData = timeElement.dataset.endTime;
       let endTimeInMinutes;
       if (endTimeData) {
-        const endTimeParts = endTimeData.split(":");
+        const endTimeParts = endTimeData.split(':');
         endTimeInMinutes = parseInt(endTimeParts[0], 10) * 60 + parseInt(endTimeParts[1], 10);
       } else {
         endTimeInMinutes = timeInMinutes + 30; // Default duration of 30 minutes if not specified
@@ -21,11 +21,11 @@ const interval = setInterval(() => {
       const startTime = timeInMinutes;
       const endTime = endTimeInMinutes;
       if (currentTime >= startTime && currentTime < endTime) {
-        timeElement.parentElement?.classList.add("highlight");
+        timeElement.parentElement?.classList.add('highlight');
       } else {
-        timeElement.parentElement?.classList.remove("highlight");
+        timeElement.parentElement?.classList.remove('highlight');
       }
     });
   });
 }, 60_000);
-window.addEventListener("unload", () => clearInterval(interval));
+window.addEventListener('unload', () => clearInterval(interval));
